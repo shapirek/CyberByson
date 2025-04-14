c69aa0aac291258d32e598ccbc2a4453b344c8c7
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-14 13:50:23 +0300

    Update and rename bot/handlers/equipment.py to bot/handlers/equipment/request.py

---
+++ bot/handlers/equipment/request.py
@@ -0,0 +1,9 @@
+def handle_request_equipment(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+
+    # Запрашиваем у пользователя информацию о технике
+    query.edit_message_text("Укажите, какая техника необходима, когда и куда ее принести:")
+
+    # Переходим в состояние REQUEST_EQUIPMENT
+    return REQUEST_EQUIPMENT
