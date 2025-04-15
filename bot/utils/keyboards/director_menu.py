fa2ba24985a211c8a18b31194c4bc0aaec541b38
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-15 20:22:20 +0300

    Create director_menu.py

---
+++ bot/utils/keyboards/director_menu.py
@@ -0,0 +1,20 @@
+async def show_director_menu(update: Update, context: CallbackContext) -> int:
+    # Проверяем, есть ли сообщение в update
+    if update.message:
+        chat_id = update.message.chat_id
+    else:
+        chat_id = update.callback_query.message.chat_id
+
+    keyboard = [
+        [InlineKeyboardButton("НОВОСТИ", callback_data='news')],
+        [InlineKeyboardButton("Сделать объявление...", callback_data='director_announcement')],
+        [InlineKeyboardButton("Написать слона/педаль", callback_data='staff_write_something')],
+        [InlineKeyboardButton("Попросить купить", callback_data='buy')],
+        [InlineKeyboardButton("Узнать контакты", callback_data='director_employee_contacts')],
+        [InlineKeyboardButton("Назад", callback_data='back_to_main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Отправляем сообщение с клавиатурой
+    await context.bot.send_message(chat_id=chat_id, text="Меню дирекции:", reply_markup=reply_markup)
+    return DIRECTOR_ACTION
