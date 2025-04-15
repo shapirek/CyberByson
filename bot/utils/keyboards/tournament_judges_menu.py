8fb544f7207724fbd810aa06f0ccea5b80ca37fa
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-15 20:27:26 +0300

    Create tournament_judges_menu.py

---
+++ bot/utils/keyboards/tournament_judges_menu.py
@@ -0,0 +1,16 @@
+async def show_tournament_judges_menu(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    await query.answer()
+
+    # Список турниров
+    tournaments = ["ФМТ", "ГУТ", "БХТ"]
+
+    # Создаем кнопки для каждого турнира
+    keyboard = [
+        [InlineKeyboardButton(tournament, callback_data=f'tournament_{tournament}')] for tournament in tournaments
+    ]
+    keyboard.append([InlineKeyboardButton("Назад", callback_data='back_to_message_options')])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(text="Выберите турнир:", reply_markup=reply_markup)
+    return CHOOSE_TOURNAMENT
