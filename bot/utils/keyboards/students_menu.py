1b1107af7c08ae31c169fcc37ee35fa313c0ae5a
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-15 20:20:12 +0300

    Create students_menu.py

---
+++ bot/utils/keyboards/students_menu.py
@@ -0,0 +1,11 @@
+async def show_students_menu(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+        [InlineKeyboardButton("Что такое КЛШ?", callback_data='student_info')],
+        [InlineKeyboardButton("Как попасть в КЛШ?", callback_data='student_how_to')],
+        [InlineKeyboardButton("Буклет этого года", callback_data='student_booklet')],
+        [InlineKeyboardButton("Архив КЛШ", callback_data='student_archive')],
+        [InlineKeyboardButton("Назад", callback_data='back_to_main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Выберите опцию для школьников:", reply_markup=reply_markup)
+    return STUDENTS_ACTION  # Возвращаем состояние меню школьников
