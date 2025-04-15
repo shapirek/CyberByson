23b46279fad0a8ed45d869fa246205a5ca6e9a9e
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-15 13:39:10 +0300

    Delete bot directory

--- bot/handlers/message/director_announcements.py
+++
@@ -1,4 +0,0 @@
-def director_announcement(update: Update, context: CallbackContext) -> int:
-    query = update.callback_query
-    query.answer()
-    return show_message_options(update, context)
--- bot/handlers/message/staff_write_message.py
+++
@@ -1,4 +0,0 @@
-def staff_write_message(update: Update, context: CallbackContext) -> int:
-    query = update.callback_query
-    query.answer()
-    return show_message_options(update, context)
