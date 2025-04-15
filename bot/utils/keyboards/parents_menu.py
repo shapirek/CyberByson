801862ce7ac3b2d4f4a85c340096c483da1eac44
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-15 22:19:18 +0300

    Update parents_menu.py

--- bot/utils/keyboards/parents_menu.py
+++ bot/utils/keyboards/parents_menu.py
@@ -1,5 +1,19 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackContext, ConversationHandler
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext
+)
 
 from bot.config import PARENTS_ACTION
 from .env import TABULA_kids
