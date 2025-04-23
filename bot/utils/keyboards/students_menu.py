d85fd07fa60844637888dc84ec732ddd364af2fe
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-23 23:35:10 +0300

    Update students_menu.py

--- bot/utils/keyboards/students_menu.py
+++ bot/utils/keyboards/students_menu.py
@@ -1,31 +1,34 @@
 from telegram import (
     Update,
-    ReplyKeyboardMarkup,
     InlineKeyboardMarkup,
-    InlineKeyboardButton
+    InlineKeyboardButton,
 )
 
 from telegram.ext import (
-    Updater,
     CommandHandler,
     MessageHandler,
     CallbackQueryHandler,
-    Filters,
     ConversationHandler,
-    CallbackContext
+    ContextTypes,
+    filters,
 )
 
 from bot.config import STUDENTS_ACTION
 
 
-async def show_students_menu(update: Update, context: CallbackContext) -> int:
+async def show_students_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     keyboard = [
         [InlineKeyboardButton("Что такое КЛШ?", callback_data='student_info')],
         [InlineKeyboardButton("Как попасть в КЛШ?", callback_data='student_how_to')],
         [InlineKeyboardButton("Буклет этого года", callback_data='student_booklet')],
         [InlineKeyboardButton("Архив КЛШ", callback_data='student_archive')],
-        [InlineKeyboardButton("Назад", callback_data='back_to_main')]
+        [InlineKeyboardButton("Назад", callback_data='back_to_main')],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Выберите опцию для школьников:", reply_markup=reply_markup)
-    return STUDENTS_ACTION  # Возвращаем состояние меню школьников
+    
+    if update.message:
+        await update.message.reply_text("Что тебя интересует?", reply_markup=reply_markup)
+    elif update.callback_query:
+        await update.callback_query.edit_message_text("Что тебя интересует?", reply_markup=reply_markup)
+
+    return STUDENTS_ACTION
