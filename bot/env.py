9235df68c9e81517bc282d82ce7d2ffa4a2454bf
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-24 22:07:23 +0300

    Create env.py

---
+++ bot/env.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # подгрузит .env в окружение
+
+TOKEN = os.getenv("BOT_TOKEN")
+TABULA = os.getenv("TABULA")
+TABULA_kids = os.getenv("TABULA_KIDS")
+SCHEDULE_SHEET_URL = os.getenv("SCHEDULE_SHEET_URL")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_LINK_PART = os.getenv("CHANNEL_LINK_PART")
+GROUP_ID = os.getenv("GROUP_ID")
+TIMEZONE = os.getenv("TIMEZONE")
