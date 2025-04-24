ed89ee7ec32dbf0489e6d1b8b6578bf73194f595
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-24 22:52:20 +0300

    Update plea.py

--- bot/services/send_messages/plea.py
+++ bot/services/send_messages/plea.py
@@ -2,7 +2,7 @@ import logging
 from typing import Optional, Dict
 
 from telegram.ext import ContextTypes
-from telegram import ParseMode
+from telegram.constants import ParseMode
 
 from bot.env import CHANNEL_ID
 from bot.services.formatters import (
