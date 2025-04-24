efa1c06f6fe7fc707455ca4dac8b6a89208bcf2f
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-24 23:30:57 +0300

    Update main_menu.py

--- bot/utils/keyboards/main_menu.py
+++ bot/utils/keyboards/main_menu.py
@@ -10,3 +10,12 @@ async def show_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text("Выберите категорию:", reply_markup=reply_markup)
+
+async def show_main_menu_in_chat(context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> None:
+    keyboard = [
+        ['Школьники', 'Сотрудники'],
+        ['Родители'],
+        ['Завершить диалог']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+    await context.bot.send_message(chat_id=chat_id, text="Выберите категорию:", reply_markup=reply_markup)
