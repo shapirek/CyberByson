8d0c1f1342f56fb71a8899516c3be328755feff6
shapirek <135975572+shapirek@users.noreply.github.com>
2025-04-24 16:06:11 +0300

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,17 @@
+from .auth_handlers import *
+from .category_handlers import *
+from .direction_handlers import *
+from .duty_handlers import *
+from .equipment_handlers import *
+from .inline_handlers import *
+from .menu_handlers import *
+from .message_handlers import *
+from .news import *
+from .parent_handlers import *
+from .problem_handlers import *
+from .recipient_handlers import *
+from .response_handlers import *
+from .team_handlers import *
+from .tournament_handlers import *
+
+__all__ = ["auth_handlers", "category_handlers", "direction_handlers", "duty_handlers", "equipment_handlers", "inline_handlers", "menu_handlers", "message_handlers", "news", "parent_handlers", "problem_handlers", "recipient_handlers", "response_handlers", "team_handlers", "tournament_handlers"]
