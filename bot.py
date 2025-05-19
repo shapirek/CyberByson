63ffebcb06956fc30e993a0df58c530f74fe2820
shapirek <135975572+shapirek@users.noreply.github.com>
2025-05-19 23:06:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -722,21 +722,22 @@ def send_message_to_channel(context: CallbackContext, message_text: str, sender_
         # Отправляем сообщение в канал
         sent_message = context.bot.send_message(
             chat_id=CHANNEL_ID,
-            text=formatted_message,  # Отправляем текст как есть, без Markdown-разметки
-            reply_to_message_id=reply_to_message_id  # Указываем thread_id, если он есть
+            text=formatted_message,
+            message_thread_id=thread_id if thread_id else None
         )
 
         # Генерируем уникальный идентификатор для этого сообщения
         unique_id = generate_unique_id()
 
         # Сохраняем данные для этого сообщения
+        # Если message_thread_id отсутствует, используем message_id как thread_id
+        thread_id = sent_message.message_thread_id or sent_message.message_id
         message_data = {
             'channel_message_id': sent_message.message_id,
-            'thread_id': sent_message.message_id,  # Сохраняем thread_id
-            'group_message_id': None  # Будет заполнено позже
+            'thread_id': thread_id,
         }
         context.user_data[unique_id] = message_data
-
+        
         # Сохраняем уникальный идентификатор в context.user_data
         context.user_data['current_message_id'] = unique_id
 
