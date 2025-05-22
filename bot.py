f3bd99dba5634887d0a4e4aed122fd366463dd8d
shapirek <135975572+shapirek@users.noreply.github.com>
2025-05-23 02:44:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ CHANNEL_ID = '-1002429128033'
 CHANNEL_LINK_PART = "2429128033"
 GROUP_ID = '-1002427182837'
 TIMEZONE = pytz.timezone('Asia/Novosibirsk')
-RENDER_EXTERNAL_HOSTNAME = 'cyberbyson-pre-alpha.onrender.com'
+RENDER_EXTERNAL_HOSTNAME = cyberbyson-pre-alpha.onrender.com
 
 CODE_INPUT = 0
 DIRECTOR_ACTION = 1
@@ -1922,7 +1922,7 @@ def main() -> None:
          url_path=TOKEN,
     )
      # Регистрируем webhook в Telegram
-     hostname = os.environ[RENDER_EXTERNAL_HOSTNAME]
+     hostname = os.environ['RENDER_EXTERNAL_HOSTNAME']
      updater.bot.set_webhook(f"https://{hostname}/{TOKEN}")
      updater.idle()
 
