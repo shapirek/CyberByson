7cff4f4923d9152996229b459e1d637d91d0c58b
shapirek <135975572+shapirek@users.noreply.github.com>
2025-07-06 02:47:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,15 +45,15 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-TOKEN = '7253322237:AAFRw8dOKC7Pus2MEsJkpFxw69z92M_BYk8'
-TABULA = 'https://docs.google.com/spreadsheets/d/13VT8JYgHeV15OVWMyquFffqGK_YBO8nPuFzf6PTMFkw/export?format=csv'
-TABULA_kids = 'https://docs.google.com/spreadsheets/d/1dsDE2ydedzEAwdtHJESx39o5Clb2cEnV5ySAGFjW_eQ/export?format=csv'
-SCHEDULE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1YOMc2k5hTNFMYeie_zbDLjMqmBRPGPKKSD8r6Tr9R9Y/export?format=csv"
-CHANNEL_ID = '-1002429128033'
-CHANNEL_LINK_PART = "2429128033"
-GROUP_ID = '-1002427182837'
-TIMEZONE = pytz.timezone('Asia/Novosibirsk')
-RENDER_EXTERNAL_HOSTNAME = 'cyberbyson-pre-alpha.onrender.com'
+TOKEN = '7253322237:AAFRw8dOKC7Pus2MEsJkpFxw69z92M_BYk8' # PASSWORD
+TABULA = 'https://docs.google.com/spreadsheets/d/13VT8JYgHeV15OVWMyquFffqGK_YBO8nPuFzf6PTMFkw/export?format=csv' # PASSWORD
+TABULA_kids = 'https://docs.google.com/spreadsheets/d/1dsDE2ydedzEAwdtHJESx39o5Clb2cEnV5ySAGFjW_eQ/export?format=csv' # PASSWORD
+SCHEDULE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1YOMc2k5hTNFMYeie_zbDLjMqmBRPGPKKSD8r6Tr9R9Y/export?format=csv" # PASSWORD
+CHANNEL_ID = '-1002429128033' # PASSWORD
+CHANNEL_LINK_PART = "2429128033" # PASSWORD
+GROUP_ID = '-1002427182837' # PASSWORD
+TIMEZONE = pytz.timezone('Asia/Novosibirsk') # PASSWORD
+RENDER_EXTERNAL_HOSTNAME = 'cyberbyson-pre-alpha.onrender.com' # PASSWORD
 
 DIRECTOR_ACTION = 1
 STAFF_ACTION = 2
